9a4bb01ccdf2585db3688e72e0e354e0afc7b06c
Philippe Kowalski <kokolkow@gmail.com>
2021-08-17 15:43:54 +0400

    Premier commit (test)

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+
+void setup(){
+
+  pinMode(GREEN_LED_PIN,OUTPUT);
+  pinMode(YELLOW_LED_PIN,OUTPUT);
+  delay(500);
+  
+  if(bootCount == 0) //Run this only the first time
+  {
+      digitalWrite(YELLOW_LED_PIN,HIGH);
+      bootCount = bootCount+1;
+  }else
+  {
+      digitalWrite(GREEN_LED_PIN,HIGH);
+  }
+  
+  delay(3000);
+
+  digitalWrite(GREEN_LED_PIN,LOW);
+  digitalWrite(YELLOW_LED_PIN,LOW);
+
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  esp_deep_sleep_start();
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // c'est un changement de test !!!
+}
